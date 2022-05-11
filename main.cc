d5f5a0b69d824e8a6aa5c5548a025a4ce073c254
Mystixs3 <67374773+Mystixs3@users.noreply.github.com>
2022-05-11 18:14:44 +0100

    Book 2 : Chapter 8 : Instances

--- main.cc
+++ main.cc
@@ -7,6 +7,8 @@
 #include "sphere.h"
 #include "moving_sphere.h"
 #include "aarect.h"
+#include "box.h"
+
 
 #include <iostream>
 
@@ -144,6 +146,16 @@ hittable_list cornell_box() {
     objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
     objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 
+	shared_ptr<hittable> box1 = make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+	box1 = make_shared<rotate_y>(box1, 15);
+	box1 = make_shared<translate>(box1, vec3(265,0,295));
+	objects.add(box1);
+
+	shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), white);
+	box2 = make_shared<rotate_y>(box2, -18);
+	box2 = make_shared<translate>(box2, vec3(130,0,65));
+	objects.add(box2);
+
     return objects;
 }
 
@@ -212,9 +224,9 @@ int main() {
         case 6:
             world = cornell_box();
             aspect_ratio = 1.0;
-            image_width = 600;
+            image_width = 400;
  			image_height = static_cast<int>(image_width / aspect_ratio);
-            samples_per_pixel = 200;
+            samples_per_pixel = 100;
             background = color(0,0,0);
             lookfrom = point3(278, 278, -800);
             lookat = point3(278, 278, 0);
