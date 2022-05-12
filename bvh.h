b733ca8f01569ec5a4b5f68e07aeccb2417e3c71
Mystixs3 <67374773+Mystixs3@users.noreply.github.com>
2022-05-12 17:39:08 +0100

    Fix bvh because that was completely broken without me knowing

--- bvh.h
+++ bvh.h
@@ -9,7 +9,7 @@
 #include <algorithm>
 
 
-class bvh_node : public hittable {
+class bvh_node : public hittable  {
     public:
         bvh_node();
 
@@ -31,6 +31,7 @@ class bvh_node : public hittable {
         shared_ptr<hittable> right;
         aabb box;
 };
+
 
 inline bool box_compare(const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis) {
     aabb box_a;
@@ -55,8 +56,9 @@ bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) 
     return box_compare(a, b, 2);
 }
 
+
 bvh_node::bvh_node(
-    std::vector<shared_ptr<hittable>>& src_objects,
+    const std::vector<shared_ptr<hittable>>& src_objects,
     size_t start, size_t end, double time0, double time1
 ) {
     auto objects = src_objects; // Create a modifiable array of the source scene objects
@@ -96,10 +98,6 @@ bvh_node::bvh_node(
     box = surrounding_box(box_left, box_right);
 }
 
-bool bvh_node::bounding_box(double time0, double time1, aabb& output_box) const {
-    output_box = box;
-    return true;
-}
 
 bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     if (!box.hit(r, t_min, t_max))
@@ -110,5 +108,12 @@ bool bvh_node::hit(const ray& r, double t_min, double t_max, hit_record& rec) co
 
     return hit_left || hit_right;
 }
+
+
+bool bvh_node::bounding_box(double time0, double time1, aabb& output_box) const {
+    output_box = box;
+    return true;
+}
+
 
 #endif
